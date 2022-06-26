654fac634afcbc5c30c213dc499c07cf130572ba
Fakhre Alam <fakhrealam9537@gmail.com>
2022-06-26 11:01:51 +0530

    deploy on mumbai testnet

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  export const nftmarketaddress = "0xA2e07a8219eAac989316ee647e691De5bF3868f6"
+  export const nftaddress = "0x2B43527d4Fd385c8Cb442bCBce8d1981089aBd48"
   
